4ec0cce3ac9880006b5bff8480716cf5d9272993
Kang Hyojun <ed@hopsoffice.com>
2023-10-27 16:37:20 +0900

    env prefix사용

--- install.sh
+++ install.sh
@@ -6,8 +6,9 @@ read -p 'Domain (default: localhost): ' domain
 read -p 'DB Name (default: hops): ' dbname
 
 cat <<EOF > .env
-DEPLOY_DOMAIN=${domain:-localhost}
-DB_USERNAME=${username:-hops}
-DB_PASSWORD=${password:-secret}
-DB_NAME=${dbname:-hops}
+HOPS_DEPLOY_DOMAIN=${domain:-localhost}
+HOPS_DB_USERNAME=${username:-hops}
+HOPS_DB_PASSWORD=${password:-secret}
+HOPS_DB_NAME=${dbname:-hops}
+HOPS_DB_DATA="./pgdata"
 EOF
