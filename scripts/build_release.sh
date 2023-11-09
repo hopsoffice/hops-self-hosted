738c7277acb36fdd0eab1533a298acfbeb9b9662
Kang Hyojun <iam.kanghyojun@gmail.com>
2023-11-10 01:01:31 +0900

    download 스크립트 추가

---
+++ scripts/build_release.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+dir="releases$@"
+rm -rf build && mkdir -p "$dir"
+cp install.sh "$dir"
+cp docker-compose.yaml "$dir"
+cp envoy.yaml "$dir"
+cp README.md "$dir"
+
+tar cvfz "$dir".tar.gz "$dir"
+rm -rf "$dir"
---
+++ scripts/download.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+tmp=$(mktemp -d)
+version="0.1"
+curl -L "https://github.com/hopsoffice/self-hosted/releases/download/$version/releases$version.tar.gz" > "$tmp/releases$version.tar.gz"
+tar xvfz "$tmp/releases$version.tar.gz"
+rm -rf $tmp
+
+install_dir="hops_self_hosted_$version"
+if [ -d "$install_dir" ]; then
+  echo -e "\n\n[Alert]\n$install_dir already exists, move to $install_dir.old\n\n"
+  mv "$install_dir" "$install_dir.old"
+fi
+mv "releases$version" "$install_dir"
+
+echo -e "\n\n[Info]\ninstalled on $install_dir"
+echo -e "You need to run install.sh & login registry before running docker compose up\n"
+echo -e "\t$ cd $install_dir"
+echo -e "\t$ ./install.sh"
+echo -e "\t$ docker login https://registry.hopsoffice.com"
+echo -e "\t$ docker compose up"
